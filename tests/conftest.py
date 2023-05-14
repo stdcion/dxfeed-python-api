b8b49c28b548de54579454109c0fefffe15376c0
Konstantin Ivaschenko <stdcion@gmail.com>
2023-05-14 19:10:51 +0300

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parents[1]))
-#from build import build_extensions
+from build import build_extensions
 
 
-# def pytest_sessionstart(session):
-#     build_extensions()
+def pytest_sessionstart(session):
+    build_extensions()
